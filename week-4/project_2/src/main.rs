3ef7bca8f7824206111f903f58c7827cde846c0d
moon01r <borke.reuben@gmail.com>
2024-11-10 08:24:50 +0100

    (Week 4 project)

--- week-4/project_2/src/main.rs
+++ week-4/project_2/src/main.rs
@@ -3,25 +3,32 @@ use std::io;
 
 fn main() 
 {
+    println!("Is Employee Experienced?: (yes/no)");
     let mut input1 = String::new();
+    io::stdin().read_line(&mut input1).expect("Wrong String Input");
+    let is_experienced = input1.trim().to_lowercase() == "yes";
 
     println!("Enter your age: ");
-    io::stdin().read_line(&mut input1).expect("Wrong String Input");
-    let age:i32 = input1.trim().parse().expect("Wrong Value Input");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Wrong String Input");
+    let age:i32 = input2.trim().parse().expect("Wrong Value Input");
 
-    let experience = true;
-    let incentive1 = 1_560_000;
-    let incentive2 = 1_480_000;
-    let incentive3 = 1_300_000;
+    let incentive: i32;
+    if is_experienced{
+        if age >= 40{
+            incentive = 1_560_000;
+            println!("The annual incentive for this employee is: N{}",incentive);
+        } else if age >= 30{
+            incentive = 1_480_000;
+            println!("The annual incentive for this employee is: N{}",incentive);
+        } else if age < 28 {
+            incentive = 1_300_000;
+            println!("The annual incentive for this employee is: N{}",incentive);
+        } else {
+            incentive = 100_00;
+            println!("The annual incentive for this employee is: N{}",incentive);
+        }
 
-    if experience == true && age >= 40 {
-        println!("Your incentive is N{}", incentive1);
-    } else if experience == true && age >= 30 && age < 40 {
-        println!("Your incentive is N{}", incentive2);
-    } else if experience == true && age < 28 {
-        println!("Your incentive is N{}", incentive3);
-    } else {
-        println!("Your Incentive is N100_000");
     }
-
+   
 }
